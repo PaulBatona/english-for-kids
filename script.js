ca5d8cb2ef91af242b0e28969dfafef28ee1b1d4
PaulBatona <60320870+PaulBatona@users.noreply.github.com>
2020-04-11 23:57:33 +0300

    feat: add burger menu
    Add styles for menu and scripts open/close for it

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const menuBtn = document.querySelector('.menu-btn');
+const menuList = document.querySelector('.menu-list');
+const overlay = document.querySelector('.menu-overlay')
 let menuOpen = false;
 
-menuBtn.addEventListener('click', () => {
+menuBtn.addEventListener('click', (e) => {
     if(!menuOpen){
         menuBtn.classList.add('open');
+        menuList.classList.remove('menu-list__hidden');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
+        menuList.classList.add('menu-list__hidden');
         menuOpen = false;
     }
 });
 
+overlay.addEventListener('click', () => {
+    menuBtn.classList.remove('open');
+    menuList.classList.add('menu-list__hidden');
+    menuOpen = false;
+});
+
+menuList.addEventListener('click', (e) => {
+    if(e.target.tagName == 'A'){
+        menuBtn.classList.remove('open');
+        menuList.classList.add('menu-list__hidden');
+        menuOpen = false;
+    }
+});
